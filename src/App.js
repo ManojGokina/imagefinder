15f0481836bd5419b4a2d96fdc2d18874814966e
manoj gokina <manojgokina@gmail.com>
2023-04-23 13:30:31 +0530

    Api integration using unsplash api.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import { debounce } from "lodash";
+import { createApi } from "unsplash-js";
+
+const unsplash = createApi({
+  accessKey: "2PIgBi88jHGVpDXy8z7_m6q2pPyUf7jrYcdPIvLFV_A",
+});
 
 function App() {
+  const [phrase, setPhrase] = useState("");
+  const phraseRef = useRef();
+  const [images, setImages] = useState([]);
+
+  const imagesRef = useRef();
+  const [fetching, setFetching] = useState(false);
+  const fetchRef = useRef(fetching);
+
+  function getUnsplashImages(query, page = 1) {
+    setFetching(true);
+    fetchRef.current = true;
+    return new Promise((resolve, reject) => {
+      unsplash.search
+        .getPhotos({
+          query,
+          page,
+          perPage: 1000,
+        })
+        .then((res) => {
+          setFetching(false);
+          fetchRef.current = false;
+          resolve(res.response.results.map((result) => result.urls.regular));
+        });
+    });
+  }
+
+  useEffect(() => {
+    phraseRef.current = phrase;
+    if (phrase !== "") {
+      debounce(() => {
+        setImages([]);
+        getUnsplashImages(phrase, 1).then((images) => {
+          setImages(images);
+          imagesRef.current = images;
+        });
+      }, 1000)();
+    }
+  }, [phrase]);
+
+  function handleScroll(e) {
+    const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
+
+    const isBottom = scrollHeight - scrollTop <= clientHeight;
+
+    if (isBottom && !fetchRef.current) {
+      getUnsplashImages(
+        phraseRef.current,
+        imagesRef.current.length / 5 + 1
+      ).then((newImgs) => {
+        imagesRef.current = [...imagesRef.current, ...newImgs];
+        setImages(imagesRef.current);
+      });
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll, { passive: true });
+
+    return document.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <h1 className="gradient-text">IMAGE FINDER</h1>
+        <p className="tagline">Discover the world through images with Image Finder</p>
+        <input
+          type="text"
+          value={phrase}
+          onChange={(e) => setPhrase(e.target.value)}
+          placeholder="What are you looking for?"
+        />
+
+        <div>{fetching && "fetching!!"}</div>
+      </div>
+      <div id="gallery">
+        {images.length > 0 &&
+          images.map((url) => (
+              <img src={url} key={url} />
+          ))}
+      </div>
     </div>
   );
 }
